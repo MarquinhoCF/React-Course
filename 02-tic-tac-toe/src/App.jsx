d7942092f95b4b3fcfac3268df36daa667158703
MarquinhoCF <marcos13@gmail.com>
2024-04-08 22:10:15 -0300

    Tic Tac Toe Project: Criando GameBoard com um grid 3x3

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,9 +1,7 @@
 import Player from './components/Player.jsx';
-
+import GameBoard from './components/GameBoard.jsx';
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
@@ -11,8 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="X"/>
           <Player initialName="Player 2" symbol="O"/>
         </ol>
-
-        GAMEBOARD
+        <GameBoard />
       </div>
     </main>
   );
