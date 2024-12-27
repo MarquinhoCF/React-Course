4796cfbb07f0c3a5055015a491c51b7c04ca05e7
Marquinho <marcos13cf@gmail.com>
2024-12-27 17:48:56 -0300

    Management App: Coletando a entrada do usuário com Refs

--- 07-management-app/src/components/NewProject.jsx
+++ 07-management-app/src/components/NewProject.jsx
@@ -1,22 +1,47 @@
+import { useRef } from "react";
+
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({ onAddProject }) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        // Validation ...
+
+        onAddProject({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate,
+        });
+    }
+
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+                <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
-                <Input
+                <Input 
+                    ref={title}
                     label="Title"
+                    type="text"
                 />
-                <Input
+                <Input 
+                    ref={description}
                     label="Description"
                     textarea
                 />
-                <Input
+                <Input 
+                    ref={dueDate} 
                     label="Due Date"
+                    type="date"
                 />
             </div>
         </div>
